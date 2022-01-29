3b37e2c67a945f1fcba8057cf2e6f80273132a0b
George Potoshin <george.potoshin@gmail.com>
2022-01-29 13:43:34 +0300

    new file:   hello_world/hello.c 	new file:   hello_world/hello.h

---
+++ hello_world/hello.h
@@ -0,0 +1,16 @@
+#ifndef hello
+#define hello
+
+
+#include <stdio.h>
+
+int
+print_hello(int a, int b)
+{
+      printf("Hello, world! %d + %d is still %d???\n",
+      a, b, a + b);
+
+      return 0;
+}
+
+#endif

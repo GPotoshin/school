ef4bad5cd48c842d70fa9bfc8ce9083054fbb783
George Potoshin <george.potoshin@gmail.com>
2022-01-29 22:50:06 +0300

    modified:   gravity/Makefile 	modified:   gravity/compare.c 	modified:   gravity/test.sh

--- gravity/compare.c
+++ gravity/compare.c
@@ -17,26 +17,26 @@
  */
 #include <stdlib.h>
 #include <stdio.h>
+#include <math.h>
 
 int main (int argc, char *argv[]) {
 	
 	double a1 = atof (argv[9]);
 	double a2 = atof (argv[21]);
 
-	double b1 = atof (argv[33]);
-	double b2 = atof (argv[45]);
-
-	
-	printf ("Comparison of 1st & 2nd methods with dt = {0.01, 0.0001} in time 10\n"\
-			"1st method:\n" \
-			"y(0.01) = %lf\n" \
-			"y(0.0001) = %lf\n" \
-			"2nd method:\n" \
-			"y(0.01) = %lf\n" \
-			"y(0.0001) = %lf\n\n" \
-			"comparison:\n" \
-			"dy1/dy2 = %lf\n" \
-			, a1, b1, a2, b2, (b2 - a2)/(b1 - a1));
+	double x = atof (argv[25]);
+	double v = atof (argv[26]);
+	double t = atof (argv[27]);
+	double y = x + v*t - 5*t*t;
+	printf ("method (dt = 0.01):\n" \
+			"y = %lf\n" \
+			"method (dt = 0.00001):\n" \
+			"y = %lf\n" \
+			"real:\n" \
+			"y = %lf\n" \
+			"power:\n" \
+			"n = 1000*dy2/dy1 = %lf\n" \
+			, a1, a2, y, fdim (0, 1000*(y-a2)/(y-a1)));
 
 	return 0;
 }
--- gravity/test.sh
+++ gravity/test.sh
@@ -2,10 +2,17 @@
 
 make compare
 
-a=$(./dif 0.1 1 10 0 10 0 10)
-b=$(./dif 0.1 2 10 0 10 0 10)
+a=$(./dif 0.01 1 10 0 10 0 1)
+b=$(./dif 0.00001 1 10 0 10 0 1)
 
-c=$(./dif 0.00001 1 10 0 10 0 10)
-d=$(./dif 0.00001 2 10 0 10 0 10)
+c=$(./dif 0.01 1 10 0 10 0 1)
+d=$(./dif 0.00001 2 10 0 10 0 1)
 
-./compare $a $b $c $d
+echo
+echo ">>> 1st method"
+echo
+./compare $a $b 0 10 1
+echo
+echo ">>> 2nd method"
+echo
+./compare $c $d 0 10 1
